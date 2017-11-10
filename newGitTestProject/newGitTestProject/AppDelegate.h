8e92117861503127714c86ce52587e4c0b76443e
Eminosoft <smdjohny.es@gmail.com>
2017-11-10 18:07:07 +0530

    > added label

---
+++ newGitTestProject/newGitTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newGitTestProject
+//
+//  Created by Eminosoft. on 10/11/17.
+//  Copyright © 2017 Eminosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newGitTestProject/newGitTestProject/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  newGitTestProject
+//
+//  Created by Eminosoft. on 10/11/17.
+//  Copyright © 2017 Eminosoft. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(30, 100, 100, 30)];
+    label.text = @"this is hello world";
+    label.textColor = [UIColor redColor];
+    
+    [self.view addSubview:label];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
